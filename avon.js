303830b5d19edded625415c7edf14e2f14ec10fa
Avon <84014668+AvonZzZ@users.noreply.github.com>
2021-09-07 23:08:00 +0500

    Create avon.js
    why not

---
+++ avon.js
@@ -0,0 +1,8 @@
+const avon = {
+  name:"avon",
+  age:"17",
+  robs:"cookies",
+  loves:["breaking JS and my bots uwu", "stealing cookies", "reading"],
+  hates:["insects", "homophobics", "racists", "etc"],
+  songs:["utaite by mafumafu", "Ignite by Alan walker", "Everytime the rain comes down by Anaa blue and Damien Dawn"]
+  }
