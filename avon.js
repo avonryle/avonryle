1a34cb9d9289e5d9a05415da0106244b28c5157e
Avon <84014668+AvonZzZ@users.noreply.github.com>
2021-09-08 21:14:41 +0500

    Update avon.js
    why not.

--- avon.js
+++ avon.js
@@ -1,8 +1,16 @@
-const avon = {
++eval const avon = {
   name:"avon",
   age:"17",
   robs:"cookies",
-  loves:["breaking JS and my bots uwu", "stealing cookies", "reading"],
-  hates:["insects", "homophobics", "racists", "etc"],
-  songs:["utaite by mafumafu", "Ignite by Alan walker", "Everytime the rain comes down by Anaa blue and Damien Dawn"]
-  }
+  loves:["breaking JS and my bots uwu", " stealing cookies", " reading"],
+  hates:["insects", " homophobics", " racists", " etc"],
+  songs:["utaite by mafumafu", " Ignite by Alan walker", " Everytime the rain comes down by Anna blue and Damien Dawn"]
+ }
+msg.channel.send(`
+                     name: ${avon.name},
+                     age: ${avon.age},
+                     robs: ${avon.robs},
+                     loves: ${avon.loves}
+                     hates: ${avon.hates},
+                     fav songs: ${avon.songs}
+                 `)
